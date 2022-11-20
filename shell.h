9ffb3242343ea0ec9fbab5e6447b9f55e0a93bef
Akpabio Emmanuel <akpabio15@gmail.com>
2022-11-20 11:30:58 -0800

    modify files

--- shell.h
+++ shell.h
@@ -1,62 +1,50 @@
-#ifndef SHELL
-#define SHELL
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <wait.h>
 #include <fcntl.h>
-#include <dirent.h>
+#include <string.h>
 #include <signal.h>
 
-/**
- * struct list - linked list for environmental variables
- * @var: holds environmental variable string
- * @next: points to next node
- */
-typedef struct list
-{
-	char *var;
-	struct list *next;
+extern char **environ;
 
-} list_t;
+/* PATH Shell Functions */
 
-/* function prototypes */
-int prompt(char **env);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-size_t get_line(char **str);
-int t_strlen(char *str, int pos, char delm);
-char *ignore_space(char *str);
-char **_str_tok(char *str, char *delm);
-char **c_str_tok(char *str, char *delm);
-char *_strcat(char *dest, char *src);
+/* Program Flow */
+
+int prompt(void);
+char *_read(void);
+char *_fullpathbuffer(char **av, char *PATH, char *copy);
+int checkbuiltins(char **av, char *buffer, int exitstatus);
+int _forkprocess(char **av, char *buffer, char *fullpathbuffer);
+
+/* String Helper Functions */
+
 char *_strdup(char *str);
-char *_strcpy(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-int _cd(char **str, list_t *env, int num);
-int built_in(char **token, list_t *env, int num, char **command);
-void non_interactive(list_t *env);
-char *_which(char *str, list_t *env);
-int __exit(char **s, list_t *env, int num, char **command);
-int _execve(char *argv[], list_t *env, int num);
-void free_double_ptr(char **str);
-void free_linked_list(list_t *list);
-int _env(char **str, list_t *env);
-char *get_env(char *str, list_t *env);
-list_t *env_linked_list(char **env);
-list_t *add_end_node(list_t **head, char *str);
-size_t print_list(list_t *h);
-int delete_nodeint_at_index(list_t **head, int index);
-int _unsetenv(list_t **env, char **str);
-int _setenv(list_t **env, char **str);
-int find_env(list_t *env, char *str);
-void not_found(char *str, int num, list_t *env);
-void cant_cd_to(char *str, int c_n, list_t *env);
-void illegal_number(char *str, int c_n, list_t *env);
-char *int_to_string(int num);
+int _splitstring(char *str);
+int _strcmp(const char *s1, const char *s2);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+
+/*Tokenize & PATH Helper Functions*/
+
+char **tokenize(char *buffer);
+int _splitPATH(char *str);
+int _PATHstrcmp(const char *s1, const char *s2);
+char *_concat(char *tmp, char **av, char *tok);
+
+/*Other Helper Funcs*/
+
+char *_getenv(const char *name);
+int _env(void);
+void _puts(char *str);
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
